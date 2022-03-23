aef7910e1c056bf6dcfcdda9c1512627a895a938
dlrlejr132 <dlrlejr132@naver.com>
2022-03-23 16:36:40 +0900

    WS Spring Day05 solved

--- work_spring/WSSpring05_GUMI06_07th_leegideok/src/main/java/com/ssafy/ws/controller/BookRestController.java
+++ work_spring/WSSpring05_GUMI06_07th_leegideok/src/main/java/com/ssafy/ws/controller/BookRestController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +42,19 @@ public class BookRestController {
 			return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 		}
 		
+		@ApiOperation(value = "{isbn}에 해당하는 도서 정보를 반환한다.", response = Book.class)
+		@GetMapping("/book/{isbn}")
+		public ResponseEntity<?> selectBoard(@PathVariable("isbn") String isbn) {
+			Book book = bService.select(isbn);
+			
+			if (book != null) {
+				return new ResponseEntity<Book>(book, HttpStatus.OK);
+			}
+			
+			return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+		}
+		
+		
 		@ApiOperation(value = "도서 정보를 저장한다.", response = Book.class)
 		@PostMapping("/book")
 		public ResponseEntity<?> insertBoard(@RequestBody Book book) {
@@ -51,5 +66,32 @@ public class BookRestController {
 			
 			return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 		}
+		
+		@ApiOperation(value = "{isbn}에 해당하는 도서 정보를 수정한다.", response = Book.class)
+		@PutMapping("/book/{isbn}")
+		public ResponseEntity<?> updateBoard(@PathVariable("isbn") String isbn, @RequestBody Book book) {
+			book.setIsbn(isbn);
+			int result = bService.update(book);
+			
+			if (result > 0) {
+				return new ResponseEntity<Book>(book, HttpStatus.OK);
+			}
+			
+			return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+		}
+		
+		@ApiOperation(value = "{isbn}에 해당하는 도서 정보를 삭제한다.", response = Integer.class)
+		@DeleteMapping("/book/{isbn}")
+		public ResponseEntity<?> deleteBoard(@PathVariable("isbn") String isbn) {
+			int result = bService.delete(isbn);
+			
+			if (result > 0) {
+				return new ResponseEntity<Integer>(result, HttpStatus.OK);
+			}
+			
+			return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+		}
+		
+		
 		
 }
