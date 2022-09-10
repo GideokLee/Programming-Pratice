59183a1e9381afeb5657a38eb943837a2db072a5
이기덕 <76939190+GideokLee@users.noreply.github.com>
2022-09-10 23:15:33 +0900

    Delete start_camp directory

--- start_camp/DebugPreTest/src/com/ssafy/school/subject/SubjectMgr.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.school.subject;
-
-public interface SubjectMgr {
-	void add(Subject v) throws DuplicatedException;
-
-	Subject[] search();
-
-	Subject search(String name) throws NotFoundException;
-}
--- start_camp/Hello/src/com/ssafy/hello/Hello.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.hello;
-
-public class Hello {
-	public static void main(String[] args) {
-		System.out.println("hello");
-	}
-}
